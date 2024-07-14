38abed8fb6016c2e8753afd29e8b4b66a0c2b97f
samurai-127 <1272447563@qq.com>
2024-07-14 09:09:48 +0800

    Site updated: 2024-07-14 09:09:46

--- js/custom.js
+++ js/custom.js
@@ -186,7 +186,7 @@ function showWelcome() {
     let c = new Date;
     a = c.getHours() >= 5 && c.getHours() < 11 ? "<span>🌤️ 早上好，一日之计在于晨</span>" : c.getHours() >= 11 && c.getHours() < 13 ? "<span>☀️ 中午好，记得午休喔~</span>" : c.getHours() >= 13 && c.getHours() < 17 ? "<span>🕞 下午好，饮茶先啦！</span>" : c.getHours() >= 17 && c.getHours() < 19 ? "<span>🚶‍♂️ 即将下班，记得按时吃饭~</span>" : c.getHours() >= 19 && c.getHours() < 24 ? "<span>🌙 晚上好，夜生活嗨起来！</span>" : "夜深了，早点休息，少熬夜";
     try {
-        document.getElementById("welcome-info").innerHTML = `欢迎来自 <b><span style="color: var(--efu-color);font-size: var(--efu-gl-size)">${s}</span></b> 的好厚米🍂<br>你是不是还在努力的<b>工作or学习</b>？<br>放下手上的工作<b>休息一下</b>吧~<br>您的IP地址为：<b><span style="font-size: 15px;">${o}</span></b><br>${a} <br>`
+        document.getElementById("welcome-info").innerHTML = `hi~欢迎我<b><span style="color: var(--efu-color);font-size: var(--efu-gl-size)">${s}</span></b>的好厚米🍂<br>你是不是还在努力的<b>工作or学习</b>？<br>放下手上的工作<b>休息一下</b>吧~<br>您的IP地址为：<b><span style="font-size: 15px;">${o}</span></b><br>${a} <br>`
     } catch (e) {
         console.log("Pjax无法获取元素")
     }
